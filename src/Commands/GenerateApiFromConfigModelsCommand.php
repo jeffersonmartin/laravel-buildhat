892411d09551dd4b3e1d9aebb3d2622e6bbe9977
Jefferson Martin <jeff@jeffersonmartin.com>
2018-07-05 11:53:50 -0700

    Initial commit of GenerateApiFromConfigModelsCommand

---
+++ src/Commands/GenerateApiFromConfigModelsCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Jeffersonmartin\Buildhat\Commands;
+
+use Jeffersonmartin\Buildhat\Common\CommandData;
+
+class GenerateApiFromConfigModelsCommand extends BaseCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'buildhat:generate-config-models';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate a CRUD API for all models in config file';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        $this->commandData = new CommandData($this, CommandData::$COMMAND_TYPE_API_SCAFFOLD);
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        // Get array of models to generate API for
+        $models = config('buildhat.models');
+
+        // Get base/root directory of Laravel application
+        $base_path = base_path();
+
+        foreach($models as $model_name => $table_name) {
+
+            // Run generator command
+            @this->line('<fg=red>Generating API for '.$model_name.'</>');
+            exec('cd '.$base_path.' && php artisan buildhat:api '.$model_name.' --fromTable --tableName='.$table_name);
+
+        }
+
+    }
+
+}
