50584c0a5f59db8a9db55ed34aae12bda2ae5404
Jefferson Martin <jeff@jeffersonmartin.com>
2018-10-28 16:33:01 -0700

    Refactor Generators/RepositoryGenerator

--- src/Generators/RepositoryGenerator.php
+++ src/Generators/RepositoryGenerator.php
@@ -37,7 +37,49 @@ class RepositoryGenerator extends BaseGenerator
             }
         }
 
+        $store_fields = [];
+
+        foreach ($this->commandData->fields as $field) {
+            if ($field->isFillable) {
+                $store_fields[] = '$$MODEL_NAME_SNAKE$->'.$field->name.' = array_get($payload, \''.$field->name.'\', null);';
+            }
+        }
+
+        $update_fields = [];
+
+        foreach ($this->commandData->fields as $field) {
+            if ($field->isFillable) {
+                $update_fields[] = '$$MODEL_NAME_SNAKE$->'.$field->name.' = array_get($payload, \''.$field->name.'\', $$MODEL_NAME_SNAKE$->'.$field->name.');';
+            }
+        }
+
+        $doc_store_param_fields = [];
+
+        foreach ($this->commandData->fields as $field) {
+            if ($field->isFillable) {
+                $doc_store_param_fields[] = $field->fieldType.'|'.$field->name;
+            }
+        }
+
+        $doc_update_param_fields = [];
+
+        foreach ($this->commandData->fields as $field) {
+            if ($field->isFillable) {
+                $doc_update_param_fields[] = $field->fieldType.'|'.$field->name;
+            }
+        }
+
         $templateData = str_replace('$FIELDS$', implode(','.buildhat_nl_tab(1, 2), $searchables), $templateData);
+
+        $templateData = str_replace('$STORE_FIELDS$', implode(buildhat_nl_tab(1, 2), $store_fields), $templateData);
+
+        $templateData = str_replace('$UPDATE_FIELDS$', implode(buildhat_nl_tab(1, 2), $update_fields), $templateData);
+
+        $templateData = str_replace('$DOC_STORE_PARAM_FIELDS$', implode(buildhat_nl_tab(1, 1).' *  '.buildhat_tabs(1), $doc_store_param_fields), $templateData);
+
+        $templateData = str_replace('$DOC_UPDATE_PARAM_FIELDS$', implode(buildhat_nl_tab(1, 1).' *  '.buildhat_tabs(1), $doc_update_param_fields), $templateData);
+
+        $templateData = fill_template($this->commandData->dynamicVars, $templateData);
 
         $docsTemplate = get_template('docs.repository', 'laravel-generator');
         $docsTemplate = fill_template($this->commandData->dynamicVars, $docsTemplate);
